0fa8868a05eae89fc4413e6ded92ca002756b120
chamberychang <sea_exp@163.com>
2022-03-30 01:26:15 +0900

    fixed

--- main.js
+++ main.js
@@ -183,8 +183,7 @@ async function commonHandle(e, context) {
     replyMsg(context, `娱乐功能说明
 明日方舟公招：--akhr[回车]--lang=语种
 抽卡模拟：[牌组名]模拟十连
-塔罗牌：翻开塔罗牌
-骰子：.r 1d100，.ra xxx 50等等`);
+骰子：.r 1d100或者.ra xxx 50等等`);
     return true;
   }
   if (context.message === '--version') {
